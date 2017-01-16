a147242ff71da45a871c108d678b92aadd81f27f
Jakub Kondrat <jkondrat@soldevelo.com>
2017-01-16 13:27:06 +0100

    OLMIS-1504: Make it possible to skip consul registration
    So we can run the template service properly without registering it with blue

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-node consul/registration.js -c register -f consul/config.json -r consul/api-definition.yaml
+
+if [ -d "consul" ]; then
+    node consul/registration.js -c register -f consul/config.json -r consul/api-definition.yaml
+else
+    echo "Warning: could not find consul directory"
+fi
 java $JAVA_OPTS -classpath extensions/*:service.jar org.springframework.boot.loader.JarLauncher && java $JAVA_OPTS -jar service.jar
