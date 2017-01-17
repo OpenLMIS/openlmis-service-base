9b500a86c16d6ae28ebc490edad1989a00876769
Jakub Kondrat <jkondrat@soldevelo.com>
2017-01-17 12:36:06 +0100

    Check if the 'extensions' volume exists
    So that services without extensions won't try to load them

--- run.sh
+++ run.sh
@@ -5,4 +5,8 @@ if [ -d "consul" ]; then
 else
     echo "Warning: could not find consul directory"
 fi
-java $JAVA_OPTS -classpath extensions/*:service.jar org.springframework.boot.loader.JarLauncher && java $JAVA_OPTS -jar service.jar
+if [ -d "extensions" ]; then
+    java $JAVA_OPTS -classpath extensions/*:service.jar org.springframework.boot.loader.JarLauncher && java $JAVA_OPTS -jar service.jar
+else
+    java $JAVA_OPTS -jar service.jar
+fi
